56c65477538854d00371bca7659bd6aee99a31e8
Jim Evans <james.h.evans.jr@gmail.com>
2022-12-13 12:04:37 -0500

    wip: Add middleware for JSON Web Token authentication

---
+++ src/middleware/tokenAuthentication.js
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken';
+import config from '../config.js';
+
+const TOKEN_COOKIE_NAME = 'pq-db-token'
+
+export default async (req, res, next) => {
+    let token = null;
+    req.user = null;
+    // The JWT can be in different places, depending on the request type.
+    // Methods of transmission not explicitly enumerated in the below code
+    // are not supported.
+    if ('token' in req.body) {
+        // Token is part of a form submitted by the user. If the
+        // token is found as part of a form sumbission, move it to
+        // an http-only cookie so future UI requests can access it.
+        token = req.body.token;
+        res.cookie(TOKEN_COOKIE_NAME, token, {
+            httpOnly: true,
+            secure: config.PQ_RUNTIME_ENVIRONMENT !== 'development'
+        });
+    } else if (TOKEN_COOKIE_NAME in req.cookies) {
+        // Token is stored as an http-only cookie. This is the usual
+        // method used by UI requests.
+        token = req.cookies[TOKEN_COOKIE_NAME];
+    } else if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        // Token is sent in the request authorizaation header. This
+        // is usually the method of transmission from an external API
+        // clients.
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (token !== null) {
+        try {
+            let decoded = await jwt.verify(token, config.PQ_SECRET_KEY);
+            req.user = { 
+                userName: decoded.userName,
+                displayName: decoded.displayName,
+                email: decoded.email,
+                sms: decoded.sms,
+                authorizationLevel: decoded.authorizationLevel
+            };
+        } catch (err) {
+            if (err.name !== 'TokenExpiredError') {
+                res.status(401).send(`{ 'error': 'Invalid token - ${JSON.stringify(err)}' }`);
+                return;
+            }
+        }
+    }
+    next();
+};
