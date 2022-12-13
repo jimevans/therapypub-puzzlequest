1022640e4228a277a0cf9349d5f61cbadb14518e
Jim Evans <james.h.evans.jr@gmail.com>
2022-12-13 18:43:08 -0500

    nit: Change to single quote

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,29 +5,59 @@ export async function login(req, res) {
         res.status(400).send(JSON.stringify({ 'error': 'No request body' }));
         return;
     }
-    if (!("userName" in req.body)) {
+    if (!('userName' in req.body)) {
         res.status(400).send(JSON.stringify({ 'error': 'No user name in request body' }));
         return;
     }
-    if (!("password" in req.body)) {
+    if (!('password' in req.body)) {
         res.status(400).send(JSON.stringify({ 'error': 'No password in request body' }));
         return;
     }
     let response = await UserService.authenticate(req.body.userName, req.body.password);
-    if ("error" in response) {
+    if ('error' in response) {
         res.status(401).send(JSON.stringify(response));
         return;
     }
     res.send(JSON.stringify(response));
 };
 
+export async function register(req, res) {
+    if (!req.body) {
+        res.status(400).send(JSON.stringify({ 'error': 'No request body' }));
+        return;
+    }
+    if (!('userName' in req.body)) {
+        res.status(400).send(JSON.stringify({ 'error': 'No user name in request body' }));
+        return;
+    }
+    if (!('password' in req.body)) {
+        res.status(400).send(JSON.stringify({ 'error': 'No password in request body' }));
+        return;
+    }
+    if (!('email' in req.body)) {
+        res.status(400).send(JSON.stringify({ 'error': 'No email in request body' }));
+        return;
+    }
+    let response = await UserService.createUser(req.body);
+    if ('error' in response) {
+        res.status(400).send(JSON.stringify(response));
+        return;
+    }
+    res.send(JSON.stringify(response));
+};
+
+function isUserAuthorized(userToBeModified, loggedInUserName, authorizationLevel) {
+    return userToBeModified === loggedInUserName || authorizationLevel > 0;
+}
+
 export async function update(req, res) {
     if (!req.body) {
         res.status(400).send(JSON.stringify({ 'error': 'No request body' }));
         return;
     }
-    // NOTE: This controller relies on middleware to validate that the
-    // user is logged in and has permissions to act on this entity.
+    if (!isUserAuthorized(req.params.userName, req.user.userName, req.user.authorizationLevel)) {
+        res.status(403).send(JSON.stringify({ 'error': `User ${req.user.userName} not authorized to update user ${req.params.userName}` }));
+    }
     let response = await UserService.updateUser(req.params.userName, req.body);
     if ('error' in response) {
         res.status(500).send(JSON.stringify(response));
@@ -36,36 +66,12 @@ export async function update(req, res) {
 };
 
 export async function remove(req, res) {
-    // NOTE: This controller relies on middleware to validate that the
-    // user is logged in and has permissions to act on this entity.
+    if (!isUserAuthorized(req.params.userName, req.user.userName, req.user.authorizationLevel)) {
+        res.status(403).send(JSON.stringify({ 'error': `User ${req.user.userName} not authorized to delete user ${req.params.userName}` }));
+    }
     let response = await UserService.deleteUser(req.params.userName);
     if ('error' in response) {
         res.status(500).send(JSON.stringify(response));
     }
     res.send(JSON.stringify(response));    
-};
-
-export async function register(req, res) {
-    if (!req.body) {
-        res.status(400).send(JSON.stringify({ 'error': 'No request body' }));
-        return;
-    }
-    if (!("userName" in req.body)) {
-        res.status(400).send(JSON.stringify({ 'error': 'No user name in request body' }));
-        return;
-    }
-    if (!("password" in req.body)) {
-        res.status(400).send(JSON.stringify({ 'error': 'No password in request body' }));
-        return;
-    }
-    if (!("email" in req.body)) {
-        res.status(400).send(JSON.stringify({ 'error': 'No email in request body' }));
-        return;
-    }
-    let response = await UserService.createUser(req.body);
-    if ("error" in response) {
-        res.status(400).send(JSON.stringify(response));
-        return;
-    }
-    res.send(JSON.stringify(response));
 };
