ee62a8e48b0c037ca637d70a439449a9f2dc0ff7
Jim Evans <james.h.evans.jr@gmail.com>
2024-07-15 11:18:25 -0400

    fix: Correctly load environment variables

--- src/config.js
+++ src/config.js
@@ -7,18 +7,17 @@ const __dirname = path.dirname(__filename);
 
 const result = environmentConfig({ path: `${__dirname}/.env` });
 
-let config;
+const config = "error" in result ? {} : result.parsed;
 
-if (!("error" in result)) {
-  console.log(`loaded config from ${__dirname}/.env`);
-  config = result.parsed;
-} else {
+if ("error" in result) {
   console.log("loaded config from process.env");
   for (const key in process.env) {
     if (key.startsWith("PQ")) {
       config[key] = process.env[key];
     }
   }
+} else {
+  console.log(`loaded config from ${__dirname}/.env`);
 }
 
 export { config };
