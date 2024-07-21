dbf67c90b258be2d81292495ce4e9561ed4db0eb
Jim Evans <james.h.evans.jr@gmail.com>
2024-07-21 12:03:14 -0400

    fix: Correct route to QR codes

--- src/routes/quest.js
+++ src/routes/quest.js
@@ -34,7 +34,7 @@ questRouter.get(
 );
 
 questRouter.get(
-  "/:name/puzzle/:puzzleName/qroode",
+  "/:name/puzzle/:puzzleName/qrcode",
   TokenAuthenticator.tokenAuthenticate,
   QuestController.renderPuzzleActivationQRCode
 )
